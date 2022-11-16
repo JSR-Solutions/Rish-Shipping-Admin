57dbb2a75d7f870b73280a02ecb192966d352bd6
Harshit Pathak <harshitasr001@gmail.com>
2022-11-17 02:26:58 +0530

    cost Api integrated

--- src/pages/Shippers/singleShipper.js
+++ src/pages/Shippers/singleShipper.js
@@ -3,7 +3,7 @@ import "./Shippers.css";
 import { useParams, useNavigate } from "react-router-dom";
 import axios from "axios";
 import { Modal, Form } from "react-bootstrap";
-import { MdModeEdit, MdDelete } from "react-icons/md";
+import { MdModeEdit, MdDelete, MdEdit } from "react-icons/md";
 function SingleShipper(props) {
   const params = useParams();
   const [shipperId, setShipperId] = useState(params.shipperId);
@@ -17,8 +17,18 @@ function SingleShipper(props) {
   const [addModal, setAddModal] = useState(false);
   const [address, setAddress] = useState("");
   const [isLoading, setIsLoading] = useState(false);
-  const [costModal,setCostModal]=useState(false);
-
+  const [costModal, setCostModal] = useState(false);
+  const [sizeCosts, setSizeCosts] = useState([]);
+  const [distanceCosts, setDistanceCosts] = useState([]);
+  const [continentCosts, setContinentCosts] = useState([]);
+  const [containerCosts, setContainerCosts] = useState([]);
+  const [weightCosts, setWeightCosts] = useState([]);
+  const [firstField,setFirstField]=useState("");
+  const [secondField, setSecondField] = useState("");
+  const [costPerUnit, setCostPerUnit] = useState("");
+  const [costType,setCostType]=useState("1");
+  const [editOpen,setEditOpen]=useState(false);
+  const [editId,setEditid]=useState("");
   const costNames = [
     "Size Cost",
     "Distance Cost",
@@ -32,6 +42,12 @@ function SingleShipper(props) {
   }, [params.shipperId]);
 
   function fetchData() {
+    setSizeCosts([]);
+    setDistanceCosts([]);
+    setWeightCosts([]);
+    setContainerCosts([]);
+    setContinentCosts([]);
+
     axios
       .get(
         `https://rish-shipping-backend-api.vercel.app/admin/api/shippers/${params.shipperId}`
@@ -39,6 +55,28 @@ function SingleShipper(props) {
       .then((res) => {
         console.log(res.data);
         setShipperData(res.data.data);
+        setSizeCosts([]);
+        setDistanceCosts([]);
+        setWeightCosts([]);
+        setContainerCosts([]);
+        setContinentCosts([]);
+        res.data.data.costs.forEach(c => {
+          if (c.costType === 1) {
+            setSizeCosts((e) => [...e, c]);
+          }
+          if (c.costType === 2) {
+            setDistanceCosts((e) => [...e, c]);
+          }
+          if (c.costType === 3) {
+            setContinentCosts((e) => [...e, c]);
+          }
+          if (c.costType === 4) {
+            setContainerCosts((e) => [...e, c]);
+          }
+          if (c.costType === 5) {
+            setWeightCosts((e) => [...e, c]);
+          }
+        });
         setIsLoading(true);
       });
   }
@@ -58,13 +96,33 @@ function SingleShipper(props) {
     setEmail("");
     setContact("");
     setWebsite("");
+    
   }
 
-  function openCost(){
+  function openCost() {
     setCostModal(true);
   }
+  function openEditCost(c){
+    setCostType(c.costType.toString());
+   
+    setEditOpen(true);
+    setEditid(c.id);
+    if(c.firstField){
+      setFirstField(c.firstField);
+    }
+    if(c.secondField){
+      setSecondField(c.secondField);
+    }
+    setCostPerUnit(c.costPerUnit);
+     setCostModal(true);
+  }
+
   function closeCost() {
     setCostModal(false);
+    setFirstField("");
+    setSecondField("");
+    setCostPerUnit("");
+    setCostType("1");
   }
 
   function openModal() {
@@ -132,6 +190,60 @@ function SingleShipper(props) {
     if (name === "website") {
       setWebsite(value);
     }
+    if (name === "firstField") {
+      setFirstField(value);
+    }
+    if (name === "secondField") {
+      setSecondField(value);
+    }
+    if (name === "costPerUnit") {
+      setCostPerUnit(value);
+    }
+    if(name==="cost"){
+      setCostType(value);
+    }
+    
+  }
+
+  function handleAddCost(){
+    axios.post(`https://rish-shipping-backend-api.vercel.app/admin/api/cost`,
+    {
+      shipper:shipperId,
+      costType:costType,
+      firstField:firstField,
+      secondField:secondField,
+      costPerUnit:costPerUnit
+    }).then((res)=>{
+      console.log("Cost Added Successfully");
+      closeCost();
+      setFirstField("");
+      setSecondField("");
+      setCostPerUnit("");
+      fetchData();
+    });
+
+  }
+  function editCost(){
+     axios
+       .patch(
+         `https://rish-shipping-backend-api.vercel.app/admin/api/cost/${editId}`,
+         {
+          costId:editId,
+           shipper: shipperId,
+           costType: costType,
+           firstField: firstField,
+           secondField: secondField,
+           costPerUnit: costPerUnit,
+         }
+       )
+       .then((res) => {
+         console.log("Cost Updated Successfully");
+          closeCost();
+          setFirstField("");
+          setSecondField("");
+          setCostPerUnit("");
+         fetchData();
+       });
   }
 
   return (
@@ -220,30 +332,301 @@ function SingleShipper(props) {
               </div>
             </div>
             <div className="details_right">
-              <h3>Cost Details</h3>
-              <button className="addCost_btn">Add New Cost</button>
-              <div className="cost_details">
-                {shipperData &&
-                  shipperData.costs.length > 0 &&
-                  shipperData.costs.map((c, key) => (
-                    <div className="cost_div">
-                    <MdDelete className="delete-icon"></MdDelete>
-                      <h5>Cost Type: {costNames[c.costType - 1]}</h5>
-                      <div style={{display:"flex"}}>
-                      {c.firstField ? <p>First Field: {c.firstField}</p> : null}
-                      {c.secondField ? (
-                        <p>Second Field: {c.secondField}</p>
+              <Modal show={costModal} onHide={closeCost}>
+                <Modal.Header closeButton>
+                  <h3>{editOpen ? "Edit Cost" : "Add Cost"}</h3>
+                </Modal.Header>
+                <Modal.Body>
+                  <div>
+                    <Form>
+                      {editOpen ? null : (
+                        <div>
+                          <Form.Label>Cost Type</Form.Label>
+                          <Form.Select name="cost" onChange={handleChange}>
+                            <option value="1">Size Cost</option>
+                            <option value="2">Distance Cost</option>
+                            <option value="3">Continent Cost</option>
+                            <option value="4">Container Cost</option>
+                          </Form.Select>
+                        </div>
+                      )}
+
+                      {costType === "1" ? (
+                        <div>
+                          <Form.Label className="mt-3">
+                            Minimum Dimension
+                          </Form.Label>
+                          <Form.Control
+                            type="text"
+                            onChange={(e) => handleChange(e)}
+                            value={firstField}
+                            name="firstField"
+                          />
+                          <Form.Label className="mt-3">
+                            Maximum Dimension
+                          </Form.Label>
+                          <Form.Control
+                            type="text"
+                            onChange={(e) => handleChange(e)}
+                            value={secondField}
+                            name="secondField"
+                          />
+                          <Form.Label className="mt-3">
+                            Cost Per Unit
+                          </Form.Label>
+                          <Form.Control
+                            type="text"
+                            onChange={(e) => handleChange(e)}
+                            value={costPerUnit}
+                            name="costPerUnit"
+                          />
+                        </div>
                       ) : null}
-                      {c.costPerUnit ? (
-                        <p>Cost Per unit: {c.costPerUnit}</p>
+                      {costType === "3" ? (
+                        <div>
+                          <Form.Label className="mt-3">From</Form.Label>
+                          <Form.Control
+                            type="text"
+                            onChange={(e) => handleChange(e)}
+                            value={firstField}
+                            name="firstField"
+                          />
+                          <Form.Label className="mt-3">To</Form.Label>
+                          <Form.Control
+                            type="text"
+                            onChange={(e) => handleChange(e)}
+                            value={secondField}
+                            name="secondField"
+                          />
+                          <Form.Label className="mt-3">
+                            Cost Per Unit
+                          </Form.Label>
+                          <Form.Control
+                            type="text"
+                            onChange={(e) => handleChange(e)}
+                            value={costPerUnit}
+                            name="costPerUnit"
+                          />
+                        </div>
                       ) : null}
+                      {costType === "2" ? (
+                        <div>
+                          <Form.Label className="mt-3">
+                            Cost Per Unit
+                          </Form.Label>
+                          <Form.Control
+                            type="text"
+                            onChange={(e) => handleChange(e)}
+                            value={costPerUnit}
+                            name="costPerUnit"
+                          />
+                        </div>
+                      ) : null}
+                      {costType === "4" ? (
+                        <div>
+                          <Form.Label className="mt-3">
+                            Container Size
+                          </Form.Label>
+                          <Form.Control
+                            type="text"
+                            onChange={(e) => handleChange(e)}
+                            value={firstField}
+                            name="firstField"
+                          />
+                          <Form.Label className="mt-3">
+                            Cost Per Unit
+                          </Form.Label>
+                          <Form.Control
+                            type="text"
+                            onChange={(e) => handleChange(e)}
+                            value={costPerUnit}
+                            name="costPerUnit"
+                          />
+                        </div>
+                      ) : null}
+
+                      <div className="add_btn">
+                        <button
+                          type="button"
+                          onClick={() =>
+                            editOpen ? editCost(editId) : handleAddCost()
+                          }
+                        >
+                          Save
+                        </button>
+                        <button
+                          className="delete_btn"
+                          type="button"
+                          onClick={() => closeCost()}
+                        >
+                          Cancel
+                        </button>
                       </div>
+                    </Form>
+                  </div>
+                </Modal.Body>
+              </Modal>
+              <h3>Cost Details</h3>
+              <button onClick={openCost} className="addCost_btn">
+                Add New Cost
+              </button>
+              <div className="cost_details">
+                {sizeCosts.length > 0 ? (
+                  <div>
+                    <h6>Size Costs</h6>
+                    <div className="table-div">
+                      <table>
+                        <tr>
+                          <th>Minimum Dimension</th>
+                          <th>Maximum Dimension</th>
+                          <th>Cost Per Unit</th>
+                          <th></th>
+                        </tr>
+                        {sizeCosts.map((c, key) => (
+                          <tr>
+                            <td>
+                              {c.firstField ? <p>{c.firstField}</p> : null}
+                            </td>
+                            <td>
+                              {c.secondField ? <p>{c.secondField}</p> : null}
+                            </td>
+                            <td>
+                              {c.costPerUnit ? <p>{c.costPerUnit}</p> : null}
+                            </td>
+                            <td>
+                              <MdModeEdit
+                                onClick={() => openEditCost(c)}
+                                className="edit_cost"
+                              />
+                            </td>
+                          </tr>
+                        ))}
+                      </table>
                     </div>
-                  ))}
+                  </div>
+                ) : null}
+                {continentCosts.length > 0 ? (
+                  <div style={{ marginTop: "20px" }}>
+                    <h6>Continent Costs</h6>
+                    <div className="table-div">
+                      <table>
+                        <tr>
+                          <th>From</th>
+                          <th>To</th>
+                          <th>Cost Per Unit</th>
+                          <th></th>
+                        </tr>
+                        {continentCosts.map((c, key) => (
+                          <tr>
+                            <td>
+                              {c.firstField ? <p>{c.firstField}</p> : null}
+                            </td>
+                            <td>
+                              {c.secondField ? <p>{c.secondField}</p> : null}
+                            </td>
+                            <td>
+                              {c.costPerUnit ? <p>{c.costPerUnit}</p> : null}
+                            </td>
+                            <td>
+                              <MdModeEdit
+                                onClick={() => openEditCost(c)}
+                                className="edit_cost"
+                              />
+                            </td>
+                          </tr>
+                        ))}
+                      </table>
+                    </div>
+                  </div>
+                ) : null}
+                {distanceCosts.length > 0 ? (
+                  <div style={{ marginTop: "20px" }}>
+                    <h6>Distance Costs</h6>
+                    <div className="table-div">
+                      <table>
+                        <tr>
+                          <th>Cost Per Unit</th>
+                          <th></th>
+                          <th></th>
+                          <th></th>
+                        </tr>
+                        {distanceCosts.map((c, key) => (
+                          <tr>
+                            <td>
+                              {c.costPerUnit ? <p>{c.costPerUnit}</p> : null}
+                            </td>
+                            <td></td>
+                            <td></td>
+                            <td>
+                              <MdModeEdit
+                                onClick={() => openEditCost(c)}
+                                className="edit_cost"
+                              />
+                            </td>
+                          </tr>
+                        ))}
+                      </table>
+                    </div>
+                  </div>
+                ) : null}
+                {containerCosts.length > 0 ? (
+                  <div style={{ marginTop: "20px" }}>
+                    <h6>Container Costs</h6>
+                    <div className="table-div">
+                      <table>
+                        <tr>
+                          <th>Container Size</th>
+                          <th>Cost Per Unit</th>
+                          <th></th>
+                        </tr>
+                        {containerCosts.map((c, key) => (
+                          <tr>
+                            <td>
+                              {c.firstField ? <p>{c.firstField}</p> : null}
+                            </td>
+
+                            <td>
+                              {c.costPerUnit ? <p>{c.costPerUnit}</p> : null}
+                            </td>
+                            <td>
+                              <MdModeEdit
+                                onClick={() => openEditCost(c)}
+                                className="edit_cost"
+                              />
+                            </td>
+                          </tr>
+                        ))}
+                      </table>
+                    </div>
+                  </div>
+                ) : null}
+                {/*  {weightCosts.length > 0 ? (
+                  <div style={{ marginTop: "20px" }}>
+                    <h6>Weight Costs</h6>
+                    <div className="table-div">
+                      <table>
+                        <tr>
+                          <th>Cost Per Unit</th>
+                          <th></th>
+                        </tr>
+                        {weightCosts.map((c, key) => (
+                          <tr>
+                            <td>
+                              {c.costPerUnit ? <p>{c.costPerUnit}</p> : null}
+                            </td>
+                            <td>
+                              <MdModeEdit />
+                            </td>
+                          </tr>
+                        ))}
+                      </table>
+                    </div>
+                  </div>
+                        ) : null}*/}
               </div>
             </div>
           </div>
-        </div> 
+        </div>
       ) : null}
     </div>
   );
